b182401164a8f153f7a8d0f24fdeec467d604bb5
GeekGhc <ghcgavin@sina.com>
2019-09-12 10:20:34 +0800

    proxy

--- Proxy/pkg/proxy.go
+++ Proxy/pkg/proxy.go
@@ -8,13 +8,13 @@ type Seller interface {
 
 // 火车站
 type Station struct {
-	stock int // 库存
+	Stock int // 库存
 }
 
 func (station *Station) Sell(name string) {
-	if station.stock > 0 {
-		station.stock--
-		fmt.Printf("代理点中： %s 买了一张票，剩余%d\n", name, station.stock)
+	if station.Stock > 0 {
+		station.Stock--
+		fmt.Printf("代理点中： %s 买了一张票，剩余%d\n", name, station.Stock)
 	} else {
 		fmt.Println("票已经售空")
 	}
@@ -22,13 +22,13 @@ func (station *Station) Sell(name string) {
 
 // 火车代理点
 type StationProxy struct {
-	station *Station // 持有一个火车站对象
+	Station *Station // 持有一个火车站对象
 }
 
 func (proxy *StationProxy) Sell(name string) {
-	if proxy.station.stock > 0 {
-		proxy.station.stock--
-		fmt.Printf("代理点： %s买了一张票，剩余: %d\n", name, proxy.station.stock)
+	if proxy.Station.Stock > 0 {
+		proxy.Station.Stock--
+		fmt.Printf("代理点： %s买了一张票，剩余: %d\n", name, proxy.Station.Stock)
 	} else {
 		fmt.Println("票已经售空")
 	}
